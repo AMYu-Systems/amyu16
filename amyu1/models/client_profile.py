3a5ccbd8611415d7a93cf4a63558d4d7891235e3
rann07 <130111107+rann07@users.noreply.github.com>
2023-05-18 14:16:06 +0800

    DMS Update Profile

--- amyu1/models/client_profile.py
+++ amyu1/models/client_profile.py
@@ -1,6 +1,3 @@
-import base64
-import datetime
-
 from odoo import models, fields, api
 
 
@@ -8,14 +5,17 @@ class EscalationContact(models.Model):
     _name = "escalation.contact"
     _description = "Escalation Point of Contact"
 
-    name = fields.Char(string='Escalation Point of Contact', required=True)
-    number_email = fields.Char(string="Contact Number and Email")
+    level = fields.Selection([('level_1', '1st Level'), ('level_2', '2nd Level'), ('level_3', '3rd Level')],
+                             string="Level")
+    contact_name = fields.Char(string='Point of Contact', required=True)
+    contact_number = fields.Char(string="Contact Number")
+    email = fields.Char(string="Email")
     escalation_id = fields.Many2one(comodel_name='client.profile', string="Escalation")
 
 
 class CorporateOfficer(models.Model):
     _name = "corporate.officer"
-    _description = "Corporate Officer"
+    _description = "Corporate Officers"
 
     name = fields.Char(string='Name', required=True)
     position = fields.Char(string='Position')
@@ -43,15 +43,15 @@ class ClientProfile(models.Model):
     _name = 'client.profile'
     _description = "Profile"
     # profile
-    assoc_id = fields.Many2one(string="Associate", comodel_name='associates.profile',required=True)
+    assoc_id = fields.Many2one(string="Associate", comodel_name='associates.profile', required=True)
     is_company = fields.Selection([('individual', 'Individual'), ('company', 'Company')], required=True)
     name = fields.Char(string="Client Name", required=True)
     image = fields.Image(string="Image")
-    organization_type = fields.Many2one(comodel_name="res.partner.category", string="Organization Type",required=True)
-    industry_class = fields.Many2one(comodel_name="res.partner.industry", string="Industry Class",required=True)
-    nature_of_business = fields.Text(string="Nature of Business")
-    date_of_engagement = fields.Date(string="Date",required=True)
-    client_id = fields.Char(string="Client ID",required=True)
+    organization_type = fields.Many2one(comodel_name="res.partner.category", string="Organization Type", required=True)
+    industry_class = fields.Many2one(comodel_name="res.partner.industry", string="Industry Class", required=True)
+    nature_of_business = fields.Text(string="Nature of Activities,Brands,Product & Services")
+    date_of_engagement = fields.Date(string="Date", required=True)
+    client_id = fields.Char(string="Client ID", required=True)
     tax_reporting_compliance = fields.Boolean(string="Tax Reporting & Compliance")
     annual_registration_update = fields.Boolean(string="Annual Registration Update")
     agree_upon_procedure = fields.Boolean(string="Agree-Upon Procedures")
@@ -87,7 +87,7 @@ class ClientProfile(models.Model):
     mobile_no2 = fields.Char(string="Mobile No.")
     email_address2 = fields.Char(string="Email Address")
     corporate_ids = fields.One2many(comodel_name='corporate.officer', inverse_name='client_profile_ids',
-                                    string="Corporate Officer")
+                                    string="Corporate Officers")
     # BIR
     vat = fields.Char(string="Tax Id No.")
     rdo_code = fields.Char(string="RDO Code")
@@ -98,11 +98,19 @@ class ClientProfile(models.Model):
     withholding_tax_expanded = fields.Boolean(string="Withholding Tax-Expanded")
     withholding_tax_compensation = fields.Boolean(string="Withholding Tax-Compensation")
     registration_fee = fields.Boolean(string="Registration Fee")
-    other_percentage_tax = fields.Char(string="Other Percentage Tax")
-    tax_type = fields.Char(string="Taxpayer Type")
-    invoice_tax = fields.Char(string="Invoice Type")
-    filing_payment = fields.Char(string="Filling & Payment")
-    books_of_account = fields.Char(string="Books of Accounts")
+    other_percentage_tax = fields.Boolean(string="Other Percentage Tax")
+    other_percentage_tax1 = fields.Char()
+    tax_type = fields.Selection([
+        ('regular', 'Regular'), ('top_5k_individual', 'Top 5k Individual'), (
+            'top_20k_corporate', 'Top 20k Corporate'), ('medium_taxpayer', 'Medium Taxpayer'), (
+            'large_taxpayer', 'Large Taxpayer')], string="Taxpayer Type")
+    invoice_tax = fields.Selection([
+        ('bound_padded', 'Bound(Padded)'), ('computer_aid_loose_leaf', 'Computer-aided(Loose-leaf)'), (
+            'cas_generated', 'CAS-Generated')], string="Invoice Type")
+    filing_payment = fields.Selection([('ebir_manual', 'eBIR(Manual)'), ('efps', 'EFPS')], string="Filling & Payment")
+    books_of_account = fields.Selection(
+        [('manual', 'Manual'), ('computer_aid_loose_leaf', 'Computer-aided(Loose-leaf)'),
+         ('cas_generated', 'CAS-Generated')], string="Books of Accounts")
     psic_psoc = fields.Char(string="PSIC/PSOC")
     ll_cas_permit_no = fields.Char(string="LL/CAS Permit No")
     ask = fields.Selection([('yes', 'Yes'), ('no', 'No')])
@@ -118,7 +126,7 @@ class ClientProfile(models.Model):
                                       string="Class of Shares")
     # Regulatory
     ask_3 = fields.Selection([('yes', 'Yes'), ('no', 'No')])
-    bureau_of_custom = fields.Boolean(string="Bureau of Custom")
+    bureau_of_custom = fields.Boolean(string="Bureau of Customs")
     bangko_sentral_pilipinas = fields.Boolean(string="Bangko Sentral ng Pilipinas")
     professional_regulation_commission = fields.Boolean(string="Professional Regulation Commission")
     philippines_council_ngo_certification = fields.Boolean(string="Philippine Council for NGO Certification")
@@ -131,7 +139,8 @@ class ClientProfile(models.Model):
     philippine_amusement_gaming_corporation = fields.Boolean(string="Philippine Amusement and Gaming Corporation")
     land_transportation_franchising_regulatory_board = fields.Boolean(
         string="Land Transportation Franchising and Regulatory Board")
-    others_ri = fields.Char(string="Others")
+    others_ri = fields.Boolean(string="Others")
+    others_reg = fields.Char()
     # SSS
     sss = fields.Char(string="SSS ER No")
     phic = fields.Char(string="PHIC ER No")
