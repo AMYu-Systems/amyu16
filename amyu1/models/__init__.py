6d0a06fe271faef3ebb208cf0236721b06927a5a
rann07 <130111107+rann07@users.noreply.github.com>
2023-08-06 11:06:09 +0800

    remove all uncommon

--- amyu1/models/__init__.py
+++ amyu1/models/__init__.py
@@ -5,7 +5,3 @@ from . import corporate_officer
 from . import class_of_shares
 from . import escalation
 from . import organization_type
-from . import supervisor_list
-from . import manager_list
-from . import partner_list
-# from . import res_partner
