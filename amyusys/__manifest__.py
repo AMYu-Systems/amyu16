57c9ca7791e6132d82ae4799e4064532fbd90071
geloiam <74101193+AngeloAlgarne@users.noreply.github.com>
2024-03-20 03:38:11 +0800

    Added module for installation convenience now and more plans

---
+++ amyusys/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "AMYU Systems",
+    'summary': """
+        Install this module to install all AMYU Systems (HRIS, CPMS, BCS, & ETS)""",
+    'description': """
+        AMYu Systems
+
+        This module holds all AMYU systems as dependencies. Install this module to install the following:
+            1. Human and Resources Information System
+            2. Client Profile Management System
+            3. Billing and Collection System
+            4. Errand Tracking System
+            
+        If possible, this module will also include test data or import functions that will automatically fill tables.
+    """,
+    'author': 'Rann Aureada & Angelo Algarne',
+    'website': "https://www.amyucpas.com",
+    'category': 'Custom',
+    'version': '0.1',
+    'depends': ['base', 'muk_web_theme', 'hrad', 'amyu1', 'bcs', 'ets'],
+
+    # always loaded
+    'data': [
+        # 'data/ets.location.csv',
+     ],
+    'application': True,
+    'license': 'LGPL-3',
+}
