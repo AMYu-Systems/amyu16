bc4cdc947da75947ab6cf0ceb6cf73dd0437f3c7
geloiam <74101193+AngeloAlgarne@users.noreply.github.com>
2024-03-25 14:37:12 +0800

    Contstraints on movement of status and state

--- bcs/models/bcs_billing.py
+++ bcs/models/bcs_billing.py
@@ -11,26 +11,26 @@ class BcsBilling(models.Model):
 
     transaction = fields.Char(string="Transaction id", readonly="1")
 
-    @api.model
-    def create(self, vals):
-        name = re.sub(r'\W+', ' ', vals['client_id.name'])
-        name_array = name.split()
-        if len(name_array) == 1:
-            transaction = name_array[0][0:3]
-        elif len(name_array) == 2:
-            name1 = name_array[0]
-            name2 = name_array[1]
-            transaction = (name1[0:2] if len(name1) >= 2 else name1[0:1]) + \
-                          (name2[0:2] if len(name1) == 1 else name2[0:1])
-        elif len(name_array) >= 3:
-            name1 = name_array[0]
-            name2 = name_array[1]
-            name3 = name_array[2]
-            transaction = name1[0:1] + name2[0:1] + name3[0:1]
-        # Compute Client ID
-        transaction += "-" + self.env['ir.sequence'].next_by_code('billing.id.seq')
-        vals['transaction'] = transaction
-        return super(BcsBilling, self).create(vals)
+    # @api.model
+    # def create(self, vals):
+    #     name = re.sub(r'\W+', ' ', vals['client_id.name'])
+    #     name_array = name.split()
+    #     if len(name_array) == 1:
+    #         transaction = name_array[0][0:3]
+    #     elif len(name_array) == 2:
+    #         name1 = name_array[0]
+    #         name2 = name_array[1]
+    #         transaction = (name1[0:2] if len(name1) >= 2 else name1[0:1]) + \
+    #                       (name2[0:2] if len(name1) == 1 else name2[0:1])
+    #     elif len(name_array) >= 3:
+    #         name1 = name_array[0]
+    #         name2 = name_array[1]
+    #         name3 = name_array[2]
+    #         transaction = name1[0:1] + name2[0:1] + name3[0:1]
+    #     # Compute Client ID
+    #     transaction += "-" + self.env['ir.sequence'].next_by_code('billing.id.seq')
+    #     vals['transaction'] = transaction
+    #     return super(BcsBilling, self).create(vals)
 
     client_id = fields.Many2one(comodel_name='client.profile', string="Client Name", required=True)
     issued_by = fields.Many2one(comodel_name='hr.employee', string="Issued By")
@@ -60,11 +60,22 @@ class BcsBilling(models.Model):
     def fsd_manager_approved_action(self):
         self.state = 'approved'
 
-    status_selection = [('not_sent', 'Not Yet Sent'),
-                        ('sent_email', 'Sent As Email'),
-                        ('send_errand', 'Send As Errand Request'),
-                        ('client_received', 'Client Has Received')]
+    status_selection = [('not_sent', 'Not yet sent'),
+                        ('sent_to_client', 'Sent to client'),
+                        ('client_received', 'Client has received')]
     status = fields.Selection(status_selection, default='not_sent')
+    
+    # only appear when status == 'sent_to_client'
+    sent_with_email = fields.Boolean(default=True, string="Sent with Email")
+    sent_with_errand = fields.Boolean(string="Sent with Errand")
+    
+    # fad has sent billing to client
+    def sent_to_client(self):
+        self.status = 'sent_to_client'
+
+    # client confirms they received it
+    def client_received(self):
+        self.status = 'client_received'
 
     # @api.constrains('state')
     # def _check_state_for_editing(self):
