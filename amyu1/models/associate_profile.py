ac28e9b3175336ecc6e79b6dec684be22aa2f4b3
rann07 <130111107+rann07@users.noreply.github.com>
2024-06-09 10:36:07 +0800

    revert again

--- amyu1/models/associate_profile.py
+++ amyu1/models/associate_profile.py
@@ -6,13 +6,13 @@ class AssociateProfile(models.Model):
     _description = "Associate Profile"
     _rec_name = 'team_id'
 
-    user_id = fields.Many2one(string="User", comodel_name='hr.employee')
-    supervisor_id = fields.Many2one(string="Supervisor", related='user_id.coach_id')
-    manager_id = fields.Many2one(string="Manager", related='user_id.parent_id')
-    team_id = fields.Many2one(related='user_id.coach_id', string="Team")
-    cluster_id = fields.Many2one(related='user_id.department_id', string="Department")
+    user_id = fields.Many2one(string="User", comodel_name='res.users', default=lambda self: self.env.user)
+    supervisor_id = fields.Many2one(string="Supervisor", comodel_name='res.users')
+    manager_id = fields.Many2one(string="Manager", comodel_name='res.users')
+    team_id = fields.Many2one(comodel_name='team.department', string="Team")
+    cluster_id = fields.Many2one(comodel_name='cluster.department', string="Department")
     image = fields.Image(string="Image")
     client_profile_ids = fields.One2many(string="Clients", comodel_name='client.profile',
                                          inverse_name="team_id")
-    job_id = fields.Many2one(string="Job Position", related='user_id.job_id')
-    lead_partner_id = fields.Many2one(string="Partner", related='user_id.executive_id')
+    job_id = fields.Many2one(string="Job Position", comodel_name='job.title')
+    lead_partner_id = fields.Many2one(string="Partner", comodel_name='res.users')
