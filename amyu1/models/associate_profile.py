ec2a1a7527f948055bd4f911b9e5e141ca4d1b78
rann07 <130111107+rann07@users.noreply.github.com>
2024-06-14 09:31:22 +0800

    Update associate_profile.py

--- amyu1/models/associate_profile.py
+++ amyu1/models/associate_profile.py
@@ -6,7 +6,7 @@ class AssociateProfile(models.Model):
     _description = "Associate Profile"
     _rec_name = 'team_id'
 
-    user_id = fields.Many2one(string="User", comodel_name='hr.employee')
+    user_id = fields.Many2one(string="User", comodel_name='res.users', default=lambda self: self.env.user)
     supervisor_id = fields.Many2one(string="Supervisor", comodel_name='res.users')
     manager_id = fields.Many2one(string="Manager", comodel_name='res.users')
     team_id = fields.Many2one(comodel_name='team.department', string="Team")
